8ebd9e587a6398c778fae78788254b1fa9ad6e9a
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-09-02 18:39:22 +0500

    Add files via upload

---
+++ Average_number.py
@@ -0,0 +1,12 @@
+number=int(input("Enter the number"))
+number1=int(input("Enter the number"))
+number2=int(input("Enter the number"))
+number3=int(input("Enter the number"))
+number_numbers=number/number
+number_numbers1=number/number
+number_numbers2=number/number
+number_numbers3=number/number
+number_numbers4=number_numbers+number_numbers2+number_numbers3+number_numbers
+number0=number+number1+number2+number3
+average_number=number0/number_numbers4
+print(average_number)
