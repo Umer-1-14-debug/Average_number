0230f35b6c8860ce47f4850c9afb1053a51f71db
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-09-02 17:46:16 +0500

    Add files via upload

---
+++ Flow_division.py
@@ -0,0 +1,4 @@
+amount=int(input("Please enter a amount of money"))
+number_of_notes=int(input("Enter the amount of notes you want to see in the amount of money you have entered"))
+division=amount//number_of_notes
+print(division)
